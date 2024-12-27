439212df5e02ef0552a5c5097caaaf5648cc0533
Aniket Singh <aniketsingh@Anikets-MacBook-Pro-2.local>
2024-12-27 20:48:09 +0530

    pricing fade up

--- src/app/pricing/page.tsx
+++ src/app/pricing/page.tsx
@@ -2,12 +2,17 @@
 import PageLayout from "@/components/PageLayout";
 import PricingSection from "@/components/PricingSection";
 import Footer from "@/components/Footer";
+import { FadeUp } from "@/components/ui/fade-up";
 
 export default function PricingPage() {
 
   return (
     <PageLayout>
+    <FadeUp delay={0.1}>
     <PricingSection />
+    </FadeUp>
+    <FadeUp delay={0.2}>
     <Footer/>
+    </FadeUp>
     </PageLayout>)
 } 
